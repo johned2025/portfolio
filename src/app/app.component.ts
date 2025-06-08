dcb4677160887cc76fc07aee9f2ad18926a0d672
John Doe <johned9321@gmail.com>
2025-06-08 13:59:32 -0700

    main html components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TechnologiesComponent } from "./components/technologies/technologies.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TechnologiesComponent, ProjectsComponent, ProfileComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
