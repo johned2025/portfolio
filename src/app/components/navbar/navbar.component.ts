32f1515db4dd6307a7998eb5f7d6ce006fcb6a56
John Doe <johned9321@gmail.com>
2025-06-12 17:24:03 -0700

    styles fixed, added personalized color theme for dark mode

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent  {
+  isDarkMode = false;
 
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.documentElement.classList.toggle('dark');
+    const theme = document.documentElement.classList.contains('dark') ? 'dark' : 'light';
+    localStorage.setItem('theme', theme);
+  }
+  
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
     "./src/**/*.{html,ts}",
   ],
+  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      primary: 'var(--color-primary)',
+      background: 'var(--color-background)',
+      text: 'var(--color-text)',
+    }
+    },
   },
   plugins: [],
+  safelist: [
+  'hover:bg-[color:var(--card-hover)]',
+  'bg-[color:var(--card-bg)]',
+  'text-[color:var(--card-text)]',
+]
 }
 
