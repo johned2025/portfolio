683dea99d81c3ced022c2de3c36150b900237d07
John Doe <johned9321@gmail.com>
2025-07-03 20:25:33 -0700

    modified 2 project cards preview

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  showModal = false;
+  selectedVideoUrl = '';
 
+  openModal(videoUrl: string): void {
+    this.selectedVideoUrl = videoUrl;
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+    this.selectedVideoUrl = '';
+  }
 }
