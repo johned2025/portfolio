c6668f42777e1ea3ea18e99ba566c06e7ca041ca
John Doe <johned9321@gmail.com>
2025-08-19 15:54:34 -0700

    reusable component project-card component was added

---
+++ src/app/components/project-card/project-card.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-project-card',
+  imports: [CommonModule],
+  templateUrl: './project-card.component.html',
+  styleUrl: './project-card.component.css'
+})
+export class ProjectCardComponent {
+  @Input() title!: string;
+  @Input() description!: string;
+  @Input() repoLink?: string;
+  @Input() mediaPath?: string;   
+  @Input() mediaType?: 'video' | 'image'; 
+
+  @Output() openModal = new EventEmitter<{ path: string, type: 'video'|'image' }>();
+  
+  handleClick() {
+    if (this.mediaPath && this.mediaType) {
+      this.openModal.emit({ path: this.mediaPath, type: this.mediaType });
+    }
+  }
+  onMouseOver(event: MouseEvent): void {
+  const target = event.currentTarget as HTMLElement | null;
+  if (target) {
+    target.style.boxShadow = 'var(--shadow-hover)';
+  }
+}
+
+onMouseOut(event: MouseEvent): void {
+  const target = event.currentTarget as HTMLElement | null;
+  if (target) {
+    target.style.boxShadow = 'var(--shadow-default)';
+  }
+}
+}
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProjectCardComponent } from '../../components/project-card/project-card.component';
 @Component({
   selector: 'app-projects',
-  imports: [CommonModule],
+  imports: [CommonModule, ProjectCardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
@@ -10,6 +11,15 @@ export class ProjectsComponent {
    showModal = false;
   selectedMediaUrl = '';
   isImage = false;
+  projects = [
+    {
+      title: 'MovieMate - Movie Recommendation Web App',
+      description: 'A movie manager that allows users to track their favorite movies and explore trending titles.',
+      repoLink: 'https://github.com/johned2025/Movie-Mate',
+      mediaPath: 'videos/movieMate.mp4',
+      mediaType: 'video' as 'video'
+    }
+  ];
 
   openModal(mediaUrl: string, type: 'image' | 'video'): void {
     this.selectedMediaUrl = mediaUrl;
