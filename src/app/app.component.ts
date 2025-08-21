6ffd138e2e328860a29a86020b8cbd05b8a0bd59
John Doe <johned9321@gmail.com>
2025-08-21 14:22:46 -0700

    added animated button to scroll top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { ProjectsComponent } from "./components/projects/projects.component";
 import { ProfileComponent } from "./components/profile/profile.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { ScrollToTopComponent } from "./components/scroll-to-top/scroll-to-top.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TechnologiesComponent, ProjectsComponent, ProfileComponent, FooterComponent, NavbarComponent],
+  imports: [RouterOutlet, TechnologiesComponent, ProjectsComponent, ProfileComponent, FooterComponent, NavbarComponent, ScrollToTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
